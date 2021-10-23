609894c55d9e1118310799633ca59a1eab31f0f7
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:59 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,18 +10,22 @@
  * @copyright xave
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "tweakURLs",             // Name
-    "Tweaks you posts URLs", // Description
-    "xave",                  // Author
+    'tweakURLs',             // Name
+    'Tweaks you posts URLs', // Description
+    'xave',                  // Author
     '1.1',                   // Version
     [
         'requires'    => [['core', '2.13']],                         // Dependencies
         'permissions' => 'usage,admin',                              // Permissions
-        'support'     => 'https://github.com/franck-paul/tweakurls', // Support URL
-        'type'        => 'plugin'                                   // Type
+        'type'        => 'plugin',                                   // Type
+
+        'details'    => 'https://open-time.net/?q=tweakurls',       // Details URL
+        'support'    => 'https://github.com/franck-paul/tweakurls', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/tweakurls/main/dcstore.xml'
     ]
 );
