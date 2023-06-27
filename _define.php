cf369f7f96fa230e92ca6118146af08a1943c7a6
franck <carnet.franck.paul@gmail.com>
2023-06-27 09:50:11 +0200

    Release 4.3.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'tweakURLs',
     'Tweaks you posts URLs',
     'xave',
-    '4.3.1',
+    '4.3.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
