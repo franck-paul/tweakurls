a3d11dc3e0afe9fc3196ab8b3253bf69af142fc6
franck <carnet.franck.paul@gmail.com>
2025-02-04 12:11:58 +0100

    Add release date information in _define.php, dcstore.xml and cope with it in github action

--- _define.php
+++ _define.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief tweakurls, a plugin for Dotclear 2
  *
@@ -16,6 +17,7 @@ $this->registerModule(
     'xave',
     '6.3.1',
     [
+        'date'        => '2003-08-13T13:42:00+0100',
         'requires'    => [['core', '2.32']],
         'permissions' => 'My',
         'type'        => 'plugin',
